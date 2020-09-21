cf75e6c06aed245cb7303d69e3d406969b559ebd
Damian Malysza <damian.malysza@gmail.com>
2020-09-21 04:08:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -58,6 +58,6 @@ function placeOrder(cardNumber) {
   } else{
     var totalCost= total()
     cart = []
-    return `Your total cost is $${total}, which will be charged to the card ${cardNumber}.`
+    return `Your total cost is $${totalCost}, which will be charged to the card ${cardNumber}.`
   }
 }
